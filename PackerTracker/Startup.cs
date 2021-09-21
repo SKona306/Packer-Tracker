7b0782655e8fc3d7b330c7b63d3b7a4d98c18004
Shaun Kent <shaunkent81@gmail.com>
2021-09-20 19:01:35 -0700

    setup AspNetCore build configuration

--- PackerTracker/Startup.cs
+++ PackerTracker/Startup.cs
@@ -1,1 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PackerTracker
+{
+  public class Startup
+  {
+    public Startup(IWebHostEnviroment env)
+    {
+      var builder = new ConfigurationBulder()
+        .SetBasePath(env.ContentRootPath)
+        .AddEnviromentVariables();
+      Configuration = builder.Build();
+    }
+
+    public IConfigurationRoot Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseRouting();
+
+      app.UseEndPoints(routes => 
+      {
+        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+      });
+    }
+  }
+}
 
